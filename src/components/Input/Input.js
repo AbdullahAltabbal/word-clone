dfc41d819e703805740455fb0f8a9455685646d5
Abdullah Al Tabbal <xlp1abode@gmail.com>
2022-11-28 16:00:08 +0100

    create a input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function InputText() {
+  const [inputValue, setInputValue] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Input: ', inputValue);
+    setInputValue('')
+  }
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value.toUpperCase());
+  };
+
+  return <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+    <label htmlFor="guess-input">Enter guess:</label>
+    <input
+
+      value={inputValue}
+      onChange={handleChange}
+      maxLength={5} id="guess-input" type="text" />
+  </form>;
+}
+
+export default InputText;
